b0414fecfd49c4f9cae791fd823e96dc584d2afb
Grusha5480 <MrGraphitem@gmail.com>
2019-05-12 22:05:35 +0300

    Added support of python-dotenv Refactoring

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from telegram.ext import InlineQueryHandler
 from telegram.ext import Updater
 from telegram import InlineQueryResultArticle
 from telegram import InputTextMessageContent
-from telegram import InlineQueryResultDocument
 from telegram import InlineQueryResultCachedSticker
 from telegram import Bot
-from datetime import date
+from datetime import datetime
 from random import randint
+from dotenv import load_dotenv
 from PIL import Image, ImageDraw, ImageFont
+import os
 
 
-# bearImage = Image.open('./bb.webp')
-CHAT_ID_ME = 205436582
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+CHAT_ID_ME = os.getenv('CHAT_ID_ME')
+ZNO_DATE = datetime.strptime(os.getenv('ZNO_DATE_ISO_STR'), '%Y-%m-%d')
 
 
 try:
@@ -26,7 +29,6 @@ except FileNotFoundError:
     f = open('ids.cnf', 'w')
     resId = 0
     f.write(str(resId))
-ZNO_DATE = date.fromisoformat("2019-05-21")
 
 
 def hello(bot, update):
@@ -36,11 +38,11 @@ def hello(bot, update):
 
 def inline(bot, update):
     global resId
-    # global bearImage
-    delta = ZNO_DATE - date.today()
+    delta = ZNO_DATE - datetime.today()
     days = delta.days
     day_rus = list(['дней', 'день', 'дня', 'дня', 'дня', 'дней', 'дней', 'дней', 'дней', 'дней', 'дней'])
-    day_eng = list()
+    # soon
+    # day_eng = list()
     day_ukr = list(['днів', 'день', 'дні', 'дні', 'дні', 'днів', 'днів', 'днів', 'днів', 'днів'])
 
     warn_rus = list(['Берегись', 'Осторожнее', 'Спасайся', 'Беги', 'Учись', 'Пора'])
@@ -48,17 +50,14 @@ def inline(bot, update):
                      'Солдат. Звучить не так і погано', 'Батько тебе породив, він тебе і вб\'є'])
 
     text = update.inline_query.query
-    lang_code = update.inline_query.from_user.language_code
 
     result = 'До ЗНО осталось {} {}, а ты {}.'.format(days, day_rus[days % 10], text)
 
     if text == '':
         warn_num = randint(0, warn_rus.__len__())
         result = 'До ЗНО осталось {} {}. {}.'.format(days, day_rus[days % 10], warn_rus[warn_num])
-    # print(text)
 
-    # result += str(resId)
-    queryResultRus = InlineQueryResultArticle(
+    query_result_rus = InlineQueryResultArticle(
         id='{}'.format(resId),
         title='Countdown Russian',
         input_message_content=InputTextMessageContent(
@@ -71,10 +70,8 @@ def inline(bot, update):
     if text == '':
         warn_num = randint(0, warn_ukr.__len__())
         result = 'До ЗНО залишилось {} {}. {}.'.format(days, day_ukr[days % 10], warn_ukr[warn_num])
-        # print(result)
 
-    # result += str(resId)
-    queryResultUkr = InlineQueryResultArticle(
+    query_result_ukr = InlineQueryResultArticle(
         id='{}'.format(resId),
         title='Countdown Ukrainian',
         input_message_content=InputTextMessageContent(
@@ -83,39 +80,40 @@ def inline(bot, update):
     )
     resId += 1
 
-    img = Image.open('beary.webp')
-    # img = Image.open('beary.webp')
+    try:
+        img = Image.open('bear.webp')
+    except IOError:
+        print('IOError')
+
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype('impact.ttf', size=100)
 
-    LINE_MAX_LENGTH = 21
+    line_max_length = 21
     res_text = ''
     wrap_num_a = 0
     wrap_num_b = 0
-    while wrap_num_a + LINE_MAX_LENGTH < result.__len__():
-        for i in range(wrap_num_a, min(wrap_num_a + LINE_MAX_LENGTH, result.__len__())):
+    while wrap_num_a + line_max_length < result.__len__():
+        for i in range(wrap_num_a, min(wrap_num_a + line_max_length, result.__len__())):
             if result[i] == ' ':
                 wrap_num_a = i + 1
             elif result[i] == '.':
                 wrap_num_a = i
             # print(wrap_num_a)
+
         res_text += result[wrap_num_b:wrap_num_a] + '\n'
         wrap_num_b = wrap_num_a
     res_text += result[wrap_num_b:]
 
-    draw.text((50, 20), font=font, text=res_text, fill=(90 + randint(0, 166), 60 + randint(0, 196),
-                                                        90 + randint(10, 20), 200))
+    draw.text((50, 20), font=font, text=res_text, fill=(0 + randint(0, 255), 0 + randint(0, 128),
+                                                        0 + randint(0, 255), 200))
     img.save('bb.webp')
-    # img.close()
     doc = open('bb.webp', 'rb')
     message = bot.send_document(chat_id=CHAT_ID_ME, document=doc)
 
-    # query_result_document_sticker = InlineQueryResultDocument()
     query_result_sticker = InlineQueryResultCachedSticker(type='sticker', id=resId, sticker_file_id=message.sticker.file_id)
     resId += 1
 
-    # print(update.inline_query.from_user)
-    update.inline_query.answer(results=list([queryResultRus, queryResultUkr, query_result_sticker]),
+    update.inline_query.answer(results=list([query_result_rus, query_result_ukr, query_result_sticker]),
                                cache_time=0,
                                is_personal=True)
 
@@ -124,8 +122,8 @@ def inline(bot, update):
     f.close()
 
 
-bot = Bot(token='token')
-updater = Updater(bot=bot)
+zno_countdownbot = Bot(token=TOKEN)
+updater = Updater(bot=zno_countdownbot)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(InlineQueryHandler(inline))
